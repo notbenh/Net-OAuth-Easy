1584886869f778c58da68784a388dfe347d335e4
ben hengst <notbenh@cpan.org>
2010-07-26 15:34:05 -0700

    starting to build out the bare bits for the object

--- lib/Net/OAuth/Easy.pm
+++ lib/Net/OAuth/Easy.pm
@@ -1,5 +1,17 @@
-use strict;
-use warnings;
 package Net::OAuth::Easy;
+use Moose;
+
+# ABSTRACT: A moose class that abstracts Net::OAuth for you
+
+has oauth => (
+   is => 'rw',
+   isa => 'Net::OAuth',
+   lazy => 1,
+   default => sub{
+      require Net::OAuth;
+      Net::OAuth->new;
+   },
+);
+
 
 1;
--- t/Net/OAuth/Easy.pm
+++ t/Net/OAuth/Easy.pm
@@ -5,6 +5,9 @@ use Fennec;
 
 tests load {
    require_ok( 'Net::OAuth::Easy' );
+   can_ok( 'Net::OAuth::Easy', qw{
+      oauth
+   });
 }
 
 1;
