541742adab1fbc4773af8924223734c7a6426148
ben hengst <notbenh@cpan.org>
2010-07-26 16:32:20 -0700

    starting to sketch out the basics

--- lib/Net/OAuth/Easy.pm
+++ lib/Net/OAuth/Easy.pm
@@ -1,17 +1,79 @@
 package Net::OAuth::Easy;
 use Moose;
+use Moose::Util::TypeConstraints;
+use File::Slurp;
 
 # ABSTRACT: A moose class that abstracts Net::OAuth for you
 
-has oauth => (
+enum 'OAuthProtocol' => qw(1.0 1.0a);
+has protocol => (
    is => 'rw',
-   isa => 'Net::OAuth',
+   isa => 'OAuthProtocol',
    lazy => 1,
-   default => sub{
-      require Net::OAuth;
-      Net::OAuth->new;
-   },
+   default => '1.0a',
 );
+
+has [qw{ consumer_key consumer_secret }] => (
+   is => 'rw',
+   isa => 'Str',
+);
+
+has [qw{ request_token_url authorize_token_url access_token_url callback }] => (
+   is => 'rw',
+   isa => 'URI', 
+);
+
+
+
+=pod
+  subtype 'Natural'
+      => as 'Int'
+      => where { $_ > 0 };
+
+  subtype 'NaturalLessThanTen'
+      => as 'Natural'
+      => where { $_ < 10 }
+      => message { "This number ($_) is not less than ten!" };
+
+  coerce 'Num'
+      => from 'Str'
+        => via { 0+$_ };
+
+=cut
+
+
+enum 'RequestMethod' => qw(GET POST);
+has request_method => (
+   is => 'rw',
+   isa => 'RequestMethod',
+   default => 'GET',
+);
+
+enum 'SignatureMethod' => qw(HMAC-SHA1 RSA-SHA1);
+has signature_method => (
+   is => 'rw',
+   isa => 'RequestMethod',
+   default => 'GET',
+);
+
+type SignatureKey => as 'Crypt::OpenSSL::RSA';
+coerce SignatureKey => 
+     from Str => 
+      via { die sprintf q{%s does not exist as a readable file} unless -r $_;
+            Crypt::OpenSSL::RSA->new_private_key( join '', read_file($_) );
+          };
+has signature_key => (
+   is => 'rw',
+   isa => 'SignatureKey',
+);
+
+
+
+sub timestamp {};
+sub nonce {};
+     
+
+
 
 
 1;
--- t/Net/OAuth/Easy.pm
+++ t/Net/OAuth/Easy.pm
@@ -6,8 +6,39 @@ use Fennec;
 tests load {
    require_ok( 'Net::OAuth::Easy' );
    can_ok( 'Net::OAuth::Easy', qw{
-      oauth
+      
+      consumer_key 
+      consumer_secret 
+
+      request_token_url 
+      authorize_token_url
+      access_token_url
+
+      request_method 
+      signature_method 
+      signature_key
+      callback 
+      protocol
+
+      timestamp 
+      nonce 
    });
 }
+
+   
+describe 'Net::OAuth::Easy' { 
+
+   my $oauth; # will contain a fresh object for each test (build via before_each)
+
+   before_each {
+      require_ok( 'Net::OAuth::Easy' );
+      $oauth = Net::OAuth::East->new;
+   }
+      
+}
+   
+
+      
+
 
 1;
